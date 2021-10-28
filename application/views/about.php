bf08cf26c59420873cd38f1c76e51b94bb6ab6d0
gitakartika25 <teresagita25@gmail.com>
2021-10-28 18:18:00 +0700

    menambahkan halaman about

---
+++ application/views/about.php
@@ -0,0 +1,36 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+      <h1>
+        About
+        <!-- <small>Control Panel</small> -->
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i></a></li>
+        <li class="active">About</li>
+      </ol>
+    </section>
+
+    <!-- Main Content -->
+    <section class="content">
+    <div class="box">
+          <div class="box-header">
+            <h3 class="box-title">Halaman About</h3>
+            <br><br>
+            <img src="assets/images/gita.jpg" height="200px" align="left" border="5"/>
+            <p > Aplikasi ini dibuat oleh :</p>
+            <h7>Nama    : Gita Kartika Pariwara</h7><br>
+            <h7>Nim     : 1931710020</h7><br>
+            <h7>Kelas   : MI-3E</h7><br>
+            <h7>Prodi   : D3 Manajemen Informatika</h7><br>
+            <h7>Tanggal : 28 Oktober 2021</h7>
+            
+        </div>
+    </div>
+       <!-- Main content -->
+    <section class="content">
+
+
+   
+    </section>
+
+    </section>
